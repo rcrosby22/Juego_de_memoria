4bc48f44b9113bd4cdc90c8d60908f21cd5299d4
Ryan Crosby <ryan.crosby22@gmail.com>
2023-05-08 08:28:26 -0500

    juego de memoria

---
+++ script.js
@@ -0,0 +1,13 @@
+/*----- constants -----*/
+let cards = [Agua, water]
+let firstCard, secondCard
+let lockBoard = false
+let score = 0
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
