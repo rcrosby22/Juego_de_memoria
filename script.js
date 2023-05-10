ecefa7e80065c5396184f3ad261874575e13558f
Ryan Crosby <ryan.crosby22@gmail.com>
2023-05-10 12:19:29 -0500

    juego de memoria

--- script.js
+++ script.js
@@ -1,119 +1,170 @@
-const cardsContainer = document.querySelector("#board")
+
 // const cards = ['dog', 'perro', 'cow', 'vaca', 'monkey', 'mono', 'chicken', 'pollo', 'rabbit', 'conejo', 'horse', 'cabello', 'bee', 'abeja', 'frog', 'rana', 'snail', 'caracol', 'snake', 'serpiente']
 
 const cards = [
   {
-  id: 0,
+  
   name: 'Dog',
-  img:'dog_1f415.png'
+  img:'images/emojis/dog_1f415.png'
   },
   {
-    id:1,
+    
     name: 'Perro',
-    img:'dog_1f415.png'
+    img:'images/emojis/dog_1f415.png'
   },
-  {
-    id:2,
+   {
     name: 'cow',
-    img:'cow_1f404.png'
-  },
-  {
-    id:3,
+     img:'images/emojis/cow_1f404.png'
+ },
+   {
+    
     name: 'vaca',
-    img:'cow_1f404.png'
-  },
-  {
-    id:4,
-    name:'Monkey',
-    img:'monkey_1f412.png'
-  },
-  {
-  id: 5,
-  name: 'Mono',
-  img:'monkey_1f412.png'
-  },
-  {
-    id:6,
-    name: 'Chicken',
-    img:'chicken_1f414.png'
-  },
-  {
-    id:7,
-    name: 'Pollo',
-    img:'chicken_1f414.png'
-  },
-  {
-    id:8,
-    name: 'Rabbit',
-    img:'rabbit_1f407.png'
-  },
-  {
-    id:9,
-    name:'Conejo',
-    img:'rabbit_1f407.png'
-  },
-  {
-  id: 10,
-  name: 'Horse',
-  img:'horse_1f40e.png'
-  },
-  {
-    id:11,
-    name: 'Cabello',
-    img:'horse_1f40e.png'
-  },
-  {
-    id:12,
-    name: 'Bee',
-    img:'honeybee_1f41d.png'
-  },
-  {
-    id:13,
-    name: 'Abeja',
-    img:'honeybee_1f41d.png'
-  },
-  {
-    id:14,
-    name:'Frog',
-    img:'frog_1f438.png'
-  },
-  {
-  id: 15,
-  name: 'Rana',
-  img:'frog_1f438.png'
-  },
-  {
-    id:16,
-    name: 'Snail',
-    img:'snail_1f40c.png'
-  },
-  {
-    id:17,
-    name: 'Caracol',
-    img:'snail_1f40c.png'
-  },
-  {
-    id:18,
-    name: 'Snake',
-    img:'snake_1f40d.png'
-  },
-  {
-    id:19,
-    name:'Serpiente',
-    img:'snake_1f40d.png'
-  }]
+    img:'images/emojis/cow_1f404.png'
+  },]
+  // {
+    
+  //   name:'Monkey',
+  //   img:'monkey_1f412.png'
+  // },
+  // {
+  
+  // name: 'Mono',
+  // img:'monkey_1f412.png'
+  // },
+  // {
+    
+  //   name: 'Chicken',
+  //   img:'chicken_1f414.png'
+  // },
+  // {
+    
+  //   name: 'Pollo',
+  //   img:'chicken_1f414.png'
+  // },
+  // {
+    
+  //   name: 'Rabbit',
+  //   img:'rabbit_1f407.png'
+  // },
+  // {
+    
+  //   name:'Conejo',
+  //   img:'rabbit_1f407.png'
+  // },
+  // {
+  
+  // name: 'Horse',
+  // img:'horse_1f40e.png'
+  // },
+  // {
+    
+  //   name: 'Cabello',
+  //   img:'horse_1f40e.png'
+  // },
+  // {
+    
+  //   name: 'Bee',
+  //   img:'honeybee_1f41d.png'
+  // },
+  // {
+    
+  //   name: 'Abeja',
+  //   img:'honeybee_1f41d.png'
+  // },
+  // {
+    
+  //   name:'Frog',
+  //   img:'frog_1f438.png'
+  // },
+  // {
+  
+  // name: 'Rana',
+  // img:'frog_1f438.png'
+  // },
+  // {
+    
+  //   name: 'Snail',
+  //   img:'snail_1f40c.png'
+  // },
+  // {
+    
+  //   name: 'Caracol',
+  //   img:'snail_1f40c.png'
+  // },
+  // {
+    
+  //   name: 'Snake',
+  //   img:'snake_1f40d.png'
+  // },
+  // {
+    
+  //   name:'Serpiente',
+  //   img:'snake_1f40d.png'
+  // }]
 
-const cardsPicklist = [...cards, ...cards]
-const cardsCount = cardsPicklist.length
+  let clicks = 0
+  let cardFlipped = []
+  
+  function checkMatch(){
+clicks = 0
+if (cardFlipped [0] == cardFlipped[1]){
+    alert('Good Match')
+}
+  cardFlipped = []
+    }
+  
 
+  function flipCard ()  {
+    let reveal = this.getAttribute('data-revealed')
+    if (reveal == 'false')
+    {
+    clicks = clicks + 1
+    let index = this.getAttribute('data-id')
+    this.setAttribute('data-revealed', true)
+    this.setAttribute('src', cards [index].img)
+    let data = this.getAttribute('data-img')
+    cardFlipped.push(data)
+    if (clicks ==2)
+    checkMatch()
+  }
+}
+
+const cardsContainer = document.querySelector("#board")
+const cardsCount = cards.length
+function buildCard () {
+  for (let I = 0; cardsCount; I++) {
+    const element = document.createElement ('div');
+    const imageEl = document.createElement('img')
+      imageEl.setAttribute('src', 'images/emojis/black.avif')
+          imageEl.classList.add('card') 
+          imageEl.classList.add('back')
+          imageEl.setAttribute('data-img',cards[I].img)
+          imageEl.setAttribute('data-id', I)
+        imageEl.setAttribute('data-name', cards[I].name)
+        imageEl.setAttribute('data-revealed', false)
+        element.appendChild(imageEl)
+      element.classList.add('card') 
+      imageEl.addEventListener('click', flipCard)
+        cardsContainer.appendChild(element)
+
+  }
+
+}
+buildCard()
+console.log(buildCard)
 
 /*----- app's state (variables) -----*/
-let revealedCount = 0
-let activeCell = null
-let endOfMove = false
+// let revealedCount = 0
+// let activeCard = null
+// let loss = false
 /*----- cached element references -----*/
 
 /*----- event listeners -----*/
+document.getElementById('restartButton').addEventListener('click', restart)
+const restart = () => {
+  location.reload()// TA question
+}
+
 
 /*----- functions -----*/
 //9Fisher-Yates Algo//
@@ -128,3 +179,6 @@ function shuffleCards() {
     cards[currentIndex] = cards[randomIndex];
     cards[randomIndex] = temporaryValue;
   }}
+   console.log(shuffleCards)
+  // for each with card and div, img
+
